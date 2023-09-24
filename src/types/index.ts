250156e3eba627dbeb47a7821f6039d80d30f4de
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-24 14:42:58 +0700

    feat: add dataListContact props

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,18 @@
 export interface AnimatedProps {
-    animation?: string;
+  animation?: string;
 }
-  
+
+export interface DataListContact {
+  id?: number;
+  created_at: string;
+  first_name: string;
+  last_name: string;
+  phones: string[];
+  __typename?: string;
+}
+
+export interface DataPhoneUser {
+  id?: number;
+  number: string;
+  __typename?: string;
+}
