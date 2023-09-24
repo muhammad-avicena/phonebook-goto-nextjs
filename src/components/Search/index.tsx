adf24ce1da285f532a54a15209efb12cb6119a49
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-24 14:41:41 +0700

    feat: add searchComponents

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { DataListContact } from '@/types';
+import SearchIcon from '@mui/icons-material/Search';
+import { searchContainer, searchInput } from '@/shared/styles';
+
+interface SearchProps {
+    data: DataListContact[];
+    onChange: (data: DataListContact[]) => void;
+}
+
+const Search = ({ data, onChange }: SearchProps) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newSearchTerm = e.target.value;
+        setSearchTerm(newSearchTerm);
+        const filteredData = data.filter((item) =>
+            item.first_name.toLowerCase().includes(newSearchTerm.toLowerCase()) ||
+            item.last_name.toLowerCase().includes(newSearchTerm.toLowerCase())
+        );
+        onChange(filteredData);
+    };
+
+    return (
+        <div css={searchContainer}>
+            <SearchIcon />
+            <input
+                css={searchInput}
+                type="text"
+                placeholder="Search by name..."
+                value={searchTerm}
+                onChange={handleInputChange}
+            />
+        </div>
+    );
+};
+
+export default Search;
---
+++ src/components/index.tsx
@@ -0,0 +1,1 @@
+export { default as SearchComponents } from './Search';
