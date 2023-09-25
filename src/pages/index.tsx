9b3dcef2dadf11e2d99d4c072f189c04d45f30d8
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-25 16:10:05 +0700

    feat: add icons, head html & desc, for better SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { bounce, AnimatedContainer, centerColumnStyle, HeaderContainer, centerStyle } from '@/shared/styles';
 import { Phone, ContactPhone, Edit } from '@mui/icons-material';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import logoIcon from "@/assets/logo.png"
 
 export default function Home() {
 
     const router = useRouter();
     return (
         <>
+            <Head>
+                <title>Phone Book App</title>
+                <meta name="description" content="Build phonebook application using react & next.js | developed by Muhammad Avicena Rahmadianto" />
+                <link rel="icon" href="/logo.png" />
+            </Head>
             <div css={centerColumnStyle}>
                 <HeaderContainer>
                     <h1><Phone /> Phone Book App</h1>
@@ -18,7 +25,7 @@ export default function Home() {
                     <Edit /> Form Contact
                 </AnimatedContainer>
                 <HeaderContainer>
-                    <p css={centerStyle}> © 2023 Muhammad Avicena. All rights reserved</p>
+                    <footer css={centerStyle}> © 2023 Muhammad Avicena. All rights reserved</footer>
                 </HeaderContainer >
             </div >
         </>
