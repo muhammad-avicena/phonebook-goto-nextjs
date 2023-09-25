f8ffed4537153386e56b7d3cc1dad342a50926ac
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-25 12:09:13 +0700

    feat: add GET_CONTACT_LIST & UPDATE_CONTACT querry

--- src/lib/queries.tsx
+++ src/lib/queries.tsx
@@ -37,6 +37,31 @@ export const DELETE_CONTACT = gql`
   }
 `;
 
+export const UPDATE_CONTACT = gql`
+mutation EditContactById($id: Int!, $_set: contact_set_input) {
+  update_contact_by_pk(pk_columns: {id: $id}, _set: $_set) {
+    id
+    first_name
+    last_name
+    phones {
+      number
+    }
+  }
+}
+`;
+
+export const GET_CONTACT_DETAILS = gql`
+query GetContactDetail($id: Int!){
+  contact_by_pk(id: $id) {
+  last_name
+  id
+  first_name
+  created_at
+  phones {
+    number
+  }
+}
+}`;
 
 export const ADD_NEW_CONTACT = gql`
 mutation AddContactWithPhones(
